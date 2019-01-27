55a3f0b866130b7f9df0b33f7ed6d414d6746c5f
Sebastian Porto <s@porto5.com>
2019-01-27 17:25:35 +1100

    Update config.fish

--- private_dot_config/private_fish/config.fish
+++ private_dot_config/private_fish/config.fish
@@ -3,3 +3,6 @@ alias doc "docker-compose"
 set -gx PATH ~/.local/bin $PATH
 set -gx PATH ~/go/bin $PATH
 set -gx PATH ~/.cargo/bin $PATH
+
+# opam configuration
+source /Users/Sebastian/.opam/opam-init/init.fish > /dev/null 2> /dev/null; or true
