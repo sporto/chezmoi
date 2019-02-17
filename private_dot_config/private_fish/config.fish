d1339a5f07b7fea39c60c9f2587ac08c34d6afb2
Sebastian Porto <s@porto5.com>
2019-02-17 11:21:32 +1100

    python

--- private_dot_config/private_fish/config.fish
+++ private_dot_config/private_fish/config.fish
@@ -3,9 +3,11 @@ alias doc "docker-compose"
 set -gx PATH ~/.local/bin $PATH
 set -gx PATH ~/go/bin $PATH
 set -gx PATH ~/.cargo/bin $PATH
+set -gx PATH ~/Library/Python/3.7/bin/ $PATH
 
 # opam configuration
 source /Users/Sebastian/.opam/opam-init/init.fish > /dev/null 2> /dev/null; or true
 
 # Direnv
 eval (direnv hook fish)
+
