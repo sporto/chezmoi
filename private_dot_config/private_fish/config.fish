cc7cbe64a0ed6d1d64fa078fea978a2cd60259fa
Sebastian <sebastian@pop-os.localdomain>
2019-07-15 10:06:10 +1000

    Add homebrew path

--- private_dot_config/private_fish/config.fish
+++ private_dot_config/private_fish/config.fish
@@ -5,6 +5,7 @@ set -gx PATH ~/bin $PATH
 set -gx PATH ~/go/bin $PATH
 set -gx PATH ~/.cargo/bin $PATH
 set -gx PATH ~/Library/Python/3.7/bin/ $PATH
+set -gx PATH /home/linuxbrew/.linuxbrew/bin $PATH
 
 # opam configuration
 source /Users/Sebastian/.opam/opam-init/init.fish > /dev/null 2> /dev/null; or true
